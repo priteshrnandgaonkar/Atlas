f86d2698bca4a46a549cfd810b990dd63fd85aa5
Pritesh Nandgaonkar <pncooldude91@gmail.com>
2017-05-20 11:39:12 +0530

    trying out edited packages

--- Sources/Atlas/Atlas.swift
+++ Sources/Atlas/Atlas.swift
@@ -2,6 +2,7 @@ public struct Country {
     public let code: String
     
     public init(code: String) {
+        print("HEY I AM THE EDITED PACKAGED")
         self.code = code.uppercased()
     }
     
@@ -13,6 +14,7 @@ public struct Country {
         let uppercaseA = UnicodeScalar("A")!
         let regionalIndicatorSymbolA = UnicodeScalar("\u{1f1e6}")!
         let distance = unicodeScalar.value - uppercaseA.value
+        //ek hi flag print karunga hamesha ho gaya ok 
         return UnicodeScalar(regionalIndicatorSymbolA.value + distance)
     }
 }
